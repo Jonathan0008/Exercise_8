0be70a3e6d7564c08b5e31458ff8b8faf52d50f2
Lovenchia <lovenchiawarouw@gmail.com>
2023-10-18 11:48:39 +0800

    adding

---
+++ src/component/Header/Generations/index.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+//     return (
+//     <.>
+//       <Gen1 />
+//       <Gen2 />
+//       <Gen3 />
+//       <Gen4 />
+//     </>
+//   );
+        return <h1> Logo</h1>;  
+  };
+  
+  export default Header;
