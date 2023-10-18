7d860075ae2811dc5f396559e62cff3084094807
Amanda <mamesahamanda09@gmail.com>
2023-10-18 18:23:20 +0800

    updateing

---
+++ src/component/Home/Card_info/index.jsx
@@ -0,0 +1,15 @@
+import Logo from "./Logo";
+import Evolution from "./Evolution";
+import Type_Effect from "./Type_Effect";
+
+const Header = () => {
+    return (
+    <>
+      <Type_Effect />
+      <Evolution />
+      <Logo/>
+    </>
+  );
+  };
+  
+  export default Header;
