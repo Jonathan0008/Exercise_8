e3a02e8916aefb5114d5a4122992d48dd36c7b80
unknown <s22210302@student.unklab.ac.id>
2023-10-31 19:16:54 +0800

    Gen

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@
 import Header from './Component/Header'
 import Home from './Component/Home'
 import Footer from './Component/Footer'
-import Logo from './Component/Header/Logo';
+import Logo from './component/Header/Logo';
+import Generations from './component/Header/Generations';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Home title= "Home"/>
       <Footer title = "Footer"/>
       <Logo title= "Logo"/>
+      <Generations title = "Generations"/>
     </>
   );
 };
